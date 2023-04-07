3789c5e56c8f87325467ed867ea2d26f4dd7e85b
George <georgemishchenko@gmail.com>
2023-04-07 16:56:03 +0200

    Website is avaible for android phones

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,43 @@
 const divs = document.querySelectorAll('.painter>div')
 const clearButton = document.querySelector('#clear-button')
+const painter = document.getElementsByClassName('painter')[0]
 
 let isButtonPressed = false
 
 document.body.onmousedown = (e) => {
-   isButtonPressed = true
-   e.preventDefault()
+	isButtonPressed = true
+	e.preventDefault()
 }
 
 document.body.onmouseup = (e) => {
-   isButtonPressed = false
-   e.preventDefault()
+	isButtonPressed = false
+	e.preventDefault()
 }
 
-document.body.addEventListener('touchstart', () => {
-   isButtonPressed = true
+painter.addEventListener('touchmove', (e) => {
+	let xPos = e.touches[0].clientX
+	let yPos = e.touches[0].clientY
+	const elem = document.elementFromPoint(xPos, yPos)
+	
+	elem.classList.add(elem.parentElement.className === 'painter' ? 'active' : '')
 })
-
-document.body.addEventListener('touchend', () => {
-   isButtonPressed = false
-})
-
 
 for (let div of divs) {
-   div.addEventListener('mouseenter', () => {
-      if (isButtonPressed) {
-         div.classList.add('active')
-      }
-   })
-
-   div.addEventListener('touchmove', () => {
-      if (isButtonPressed) {
-         div.classList.add('active')
-      }
-   })
+	div.onmouseenter = () => {
+		if (isButtonPressed) {
+			div.classList.add('active')
+		}
+	}
 }
-
 
 clearButton.onclick = () => {
-   const input = []
-   for (let div of divs) {
-      input.push(div.className === 'active' ? 1 : 0)
-      div.classList = []
-   }
-   console.log(input)
-   navigator.clipboard.writeText(input.toString())
+	const input = []
+	for (let div of divs) {
+		input.push(div.className === 'active' ? 1 : 0)
+		div.classList = []
+	}
+	console.log(input)
+	navigator.clipboard.writeText(input.toString())
 }
+
+document.body.addEventListener('touchstart', function(e){ e.preventDefault();});
